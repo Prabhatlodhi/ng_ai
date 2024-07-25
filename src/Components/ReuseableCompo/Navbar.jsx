26fb8dc0ce05dceb9ca2a9a739ee8a3ea5a59adc
Prabhatlodhi <lodhi.prabhat@gmail.com>
2024-07-25 14:29:14 +0530

    logout btn added

--- src/Components/ReuseableCompo/Navbar.jsx
+++ src/Components/ReuseableCompo/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 import { Logout } from "../../utils/Logout";
 import "./Navbar.css";
@@ -6,8 +7,14 @@ import "./Navbar.css";
 
 const Navbar = () => {
   const location = useLocation();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
   const auth = JSON.parse(localStorage.getItem("AUTH"));
   const profilePicture = auth?.profile_picture || assets.Student;
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <div className="nav">
       <div className="nav-options">
@@ -20,9 +27,18 @@ const Navbar = () => {
         {/* <Link to={"/main"} style={{textDecoration:"none"}}><span>G-ni</span></Link> */}
         {/* <Link to={"/Experi"} style={{textDecoration:"none"}}><span>Experi</span></Link> */}
       </div>
-      <div className="profile-picture" >
-        {/* <Tooltip id="my-tooltip" /> */}
-        <img src={profilePicture} alt="Student" onClick={Logout} />
+      <div className="profile-section">
+        <img
+          src={profilePicture}
+          alt="Student"
+          onClick={toggleDropdown}
+          className="profile-picture"
+        />
+        {dropdownVisible && (
+          <div className="dropdown-menu">
+            <span onClick={Logout}>Logout</span>
+          </div>
+        )}
       </div>
     </div>
   );
